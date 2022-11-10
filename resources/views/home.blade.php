783854aca81843b26b9b4a7d26d0fa5dc15a4bf6
Daniel Sepulveda <cdsi21_@hotmail.com>
2022-11-10 10:08:20 -0700

    Actividad: implementar plantilla, uso de extends, section, yield

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=h1, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Bienvenido {{Auth::user()->name ?? "Sin nombre"}}</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('contenido')
+    <h1>
+        Hola crayola
+    </h1>
+@endsection
+
+@section('scripts')
+    {{-- Comentario en scripts --}}
+@endsection
